0a56cd31e1f6cbff7b6fb376702b0a8ba0e69b5e
imba-tjd <109224573@qq.com>
2024-01-20 21:15:27 +0800

    io.Reader

--- pearch.go
+++ pearch.go
@@ -4,6 +4,7 @@ package pearch
 import (
 	"encoding/binary"
 	"errors"
+	"io"
 	"os"
 )
 
@@ -12,8 +13,8 @@ const PE_POINTER_OFFSET = 60
 const MACHINE_VALUE_I386 = 0x014c
 const MACHINE_VALUE_X64 = 0x8664
 
-// GetTargetMachineValue reads the file so that f can't be reused
-func GetTargetMachineValue(f *os.File) (uint16, error) {
+// GetTargetMachineValue reads the file, so that f can't be reused
+func GetTargetMachineValue(f io.Reader) (uint16, error) {
 	var b [4096]byte
 	_, err := f.Read(b[:])
 	if err != nil {
@@ -22,13 +23,13 @@ func GetTargetMachineValue(f *os.File) (uint16, error) {
 
 	PE_HEADER_ADDR := binary.LittleEndian.Uint32(b[PE_POINTER_OFFSET:])
 	if PE_HEADER_ADDR+MACHINE_OFFSET+2 > 4096 {
-		return 0, errors.New("goarch: failed to find PE_HEADER_ADDR in the first 4096 bytes.")
+		return 0, errors.New("goarch: failed to find PE_HEADER_ADDR in the first 4096 bytes")
 	}
 
 	return binary.LittleEndian.Uint16(b[PE_HEADER_ADDR+MACHINE_OFFSET:]), nil
 }
 
-func IsX86(filepath string) bool {
+func mustGetTargetMachineValueByFilepath(filepath string) uint16 {
 	f, err := os.Open(filepath)
 	if err != nil {
 		panic(err)
@@ -40,20 +41,13 @@ func IsX86(filepath string) bool {
 		panic(err)
 	}
 
-	return v == MACHINE_VALUE_I386
+	return v
+}
+
+func IsX86(filepath string) bool {
+	return mustGetTargetMachineValueByFilepath(filepath) == MACHINE_VALUE_I386
 }
 
 func IsX64(filepath string) bool {
-	f, err := os.Open(filepath)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	v, err := GetTargetMachineValue(f)
-	if err != nil {
-		panic(err)
-	}
-
-	return v == MACHINE_VALUE_X64
+	return mustGetTargetMachineValueByFilepath(filepath) == MACHINE_VALUE_X64
 }
